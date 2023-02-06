e81890186b90b81db96b23e03755bef7c5b79b71
RizkiMufrizal <mufrizalrizki@gmail.com>
2023-02-06 23:14:21 +0700

    initial project

--- src/main/java/org/rizki/mufrizal/belajar/aop/aspect/LoggingAspect.java
+++ src/main/java/org/rizki/mufrizal/belajar/aop/aspect/LoggingAspect.java
@@ -1,21 +1,17 @@
 package org.rizki.mufrizal.belajar.aop.aspect;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Slf4j
 public class LoggingAspect {
-    @Before(value = "@annotation(org.rizki.mufrizal.belajar.aop.annotation.Logging)")
-    public void getAccountOperationInfo(JoinPoint joinPoint) {
-        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-
-        System.out.println("full method description: " + signature.getMethod());
-        System.out.println("method name: " + signature.getMethod().getName());
-        System.out.println("declaring type: " + signature.getDeclaringType());
-
+    @AfterReturning(value = "@annotation(org.rizki.mufrizal.belajar.aop.annotation.Logging)", returning = "returnValue")
+    public void getAccountOperationInfo(JoinPoint joinPoint, Object returnValue) {
+        log.info("cek {}", returnValue);
     }
 }
